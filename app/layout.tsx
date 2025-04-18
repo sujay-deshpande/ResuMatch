b206d002de822cdc8efc89e4dd3ce9d86d83259f
sujay-deshpande <sujaydeshpande2406@gmail.com>
2025-04-18 13:21:29 +0530

    Pushing V1.0.7

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -8,7 +10,7 @@ export const metadata: Metadata = {
   title: 'ResuMatch: Professionalism Matching',
   description: 'ResuMatch is a fun project that playfully matches your resume and skills with your crush’s to see how compatible you’d be in the corporate world.',
   icons: {
-    icon: '/logo.ico', // relative to /public
+    icon: '/logo.ico', 
   },
 };
 
@@ -20,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children} <Analytics /> <SpeedInsights/></body>
     </html>
   );
 }
